e3db3b003b95ebc29443dc619b6c53ed39c800ee
jalil-1 <37008398+jalil-1@users.noreply.github.com>
2019-09-04 21:10:25 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,143 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Platform,
+  KeyboardAvoidingView,
+  ImageBackground,
+  ActivityIndicator,
+  StatusBar,
+} from 'react-native';
+
+import weather from './Utils/getImageForWeather';
+import { fetchLocationId, fetchWeather } from './Utils/api';
+import SearchInput from './Components/SearchInput';
+import getImageForWeather from './Utils/getImageForWeather';
+
+
+export default class App extends React.Component {
+
+  state = {
+    loading: false,
+    error: false,
+    location: '',
+    temperature: 0,
+    weather: "Clear",
+  };
+
+  componentDidMount() {
+    this.handleUpdateLocation('Accra');
+  }
+
+
+  handleUpdateLocation = async city => {
+    if (!city) return;
+
+    this.setState({ loading: true }, async () => {
+      try {
+        const locationId = await fetchLocationId(city);
+        const { location, weather, temperature } = await fetchWeather(
+          locationId,
+        );
+        this.setState({
+          loading: false,
+          error: false,
+          location,
+          weather,
+          temperature,
+        });
+      } catch (e) {
+        this.setState({
+          loading: false,
+          error: true,
+        });
+      }
+    });
+  };
+
+
+  render() {
+    const { loading, error, location, weather, temperature, } = this.state;
+    return (
+      <KeyboardAvoidingView style={styles.container} behavior="padding"
+      >
+        
+        <ImageBackground source={getImageForWeather(weather)}
+          style={styles.imageContainer}
+          imageStyle={styles.image}
+        >
+
+          <View style={styles.detailsContainer}>
+          <StatusBar barStyle="light-content" />
+            <ActivityIndicator animating={loading} color="white" size="large" />
+            {!loading && (
+              <View>
+
+                {error && (
+                  <Text style={[styles.smallText, styles.textStyle]}> WELCOME TO JAYS WEATHER UPDATE! </Text>
+                )}
+                {!error && (<View>
+                  <Text style={[styles.largeText, styles.textStyle]}
+                  >
+                    {location}
+                  </Text>
+                  <Text style={[styles.smallText, styles.textStyle]}
+                  >
+                    {weather}
+                  </Text>
+                  <Text style={[styles.largeText, styles.textStyle]}
+                  >
+                    {`${Math.round(temperature)}°`}
+                  </Text>
+                </View>
+                )}
+                <SearchInput 
+                placeholder="Search any city" 
+                onSubmit={this.handleUpdateLocation}
+                />
+
+              </View>
+            )}
+          </View>
+        </ImageBackground>
+      </KeyboardAvoidingView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#34495E',
+
+  },
+
+  detailsContainer: {
+    flex: 1, justifyContent: 'center', backgroundColor: 'rgba(0,0,0,0.2)', paddingHorizontal: 20,
+  },
+
+  textStyle: {
+    textAlign: 'center',
+    fontFamily: Platform.OS === 'ios' ? 'AvenirNext-Regular' : 'Roboto', color: 'white',
+  },
+
+  largeText: {
+    fontSize: 44,
+  },
+  smallText: {
+    fontSize: 18,
+
+  },
+  imageContainer: {
+    flex: 1
+  },
+  image: {
+    flex: 1,
+    width: null,
+    height: null,
+    resizeMode: "cover"
+  },
+
+
+});
